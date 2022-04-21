927dceb74e198d74875191f7c3b065ff70f1d96b
Artur Abzalov <mr.art-abzalov@yandex.ru>
2022-04-21 09:42:04 +0300

    add class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanCopy = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Compare ref Helloworld: "+(beanCopy==bean));
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catCopy = (Cat) applicationContext.getBean("cat");
+        System.out.println("Compare ref Cat: "+(catCopy==cat));
     }
 }
